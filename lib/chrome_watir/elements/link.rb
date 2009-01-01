97177f2eb0b985e7ed399c7ca051c99776588cbb
JijeshMohan <JijeshMohan@3b2c9f30-d591-11dd-86d7-074df07e0730>
2009-01-01 08:16:49 +0000

    added text property for link object.

--- lib/chrome_watir/elements/link.rb
+++ lib/chrome_watir/elements/link.rb
@@ -1,4 +1,10 @@
 module ChromeWatir
   class Link < WebElement
+    def text
+      assert_exist
+      @container.js_eval("element.innerText")
+      return @container.read_socket
+    end
+    
   end
 end
--- test/test_link.rb
+++ test/test_link.rb
@@ -12,4 +12,7 @@ class LinkTest < Test::Unit::TestCase
     @browser.back
     @browser.link(:link_text, "Image and a text link").click
   end
+  def test_link_text
+   assert_equal("test1",@browser.link(:link_text,"test1").text)
+  end
 end
