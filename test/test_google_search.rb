66e030910817a3a6da121d1a0a93af66c178086b
saidesertrose2004 <saidesertrose2004@3b2c9f30-d591-11dd-86d7-074df07e0730>
2009-01-06 09:22:41 +0000

    Fixed GoogleSearchTest

--- test/test_google_search.rb
+++ test/test_google_search.rb
@@ -10,11 +10,12 @@ class GoogleSearchTest < Test::Unit::TestCase
     @browser.close
   end
   def test_do_advanced_search
-    @browser.link(:link_text, "Advanced Search").click
+    @browser.link(:text, "Advanced Search").click
     @browser.text_field(:id, "as_oq0").set "watir"
     @browser.text_field(:id, "as_oq1").set "ruby"
     @browser.text_field(:id, "as_oq2").set "testing"
-#    @browser.radio(:id, "ss").set
+    @browser.radio(:id, "ss").set
     @browser.button(:value, "Advanced Search").click
+    assert_equal("watir OR ruby OR testing - Google Search",@browser.title)
   end
 end
