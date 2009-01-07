35c1fef342e7007a85f5283bf03a1591a0e72d99
saidesertrose2004 <saidesertrose2004@3b2c9f30-d591-11dd-86d7-074df07e0730>
2009-01-07 11:10:11 +0000

    Refactored the Radio and CheckBox elements

--- lib/chrome_watir/elements/check_box.rb
+++ lib/chrome_watir/elements/check_box.rb
@@ -1,27 +1,5 @@
 module ChromeWatir
-  class CheckBox < InputElement
-    ELEMENT_TYPE = "input"
+  class CheckBox < InputOptionElement
     INPUT_TYPE = ["checkbox"]
-    def set
-      assert_enabled
-      @container.js_eval("element.checked = true")
-      @container.read_socket
-    end
-    def clear
-      assert_enabled
-      @container.js_eval("element.checked = false")
-      @container.read_socket
-    end
-    def checked?
-      assert_exist
-      @container.js_eval("element.checked")
-      value = @container.read_socket
-      if value.eql? "true"
-        return true
-      end
-      if value.eql? "false"
-        return false
-      end
-    end
   end
 end
--- lib/chrome_watir/elements/radio.rb
+++ lib/chrome_watir/elements/radio.rb
@@ -1,27 +1,5 @@
 module ChromeWatir
-  class Radio < InputElement
-    ELEMENT_TYPE = "input"
+  class Radio < InputOptionElement
     INPUT_TYPE = ["radio"]    
-    def set
-      assert_enabled
-      @container.js_eval("element.checked = true")
-      @container.read_socket
-    end
-    def clear
-      assert_enabled
-      @container.js_eval("element.checked = false")
-      @container.read_socket
-    end
-    def checked?
-      assert_exist
-      @container.js_eval("element.checked")
-      value = @container.read_socket
-      if value.eql? "true"
-        return true
-      end
-      if value.eql? "false"
-        return false
-      end
-    end
   end
 end
