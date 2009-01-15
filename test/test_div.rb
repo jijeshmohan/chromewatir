4ba2e9d51f72816e4f822bc21bf43b010513be45
JijeshMohan <JijeshMohan@3b2c9f30-d591-11dd-86d7-074df07e0730>
2009-01-15 04:15:52 +0000

    updated the div test with test_inside_div

--- test/test_div.rb
+++ test/test_div.rb
@@ -4,10 +4,23 @@ class DivTest < Test::Unit::TestCase
   def teardown
     @browser.close
   end
-  def test_div_exist
+  def setup
     @browser = start_browser("div") 
+  end
+  def test_div_exist 
     assert_equal(true,@browser.div(:id, "div1").exist?)
     assert_equal(false,@browser.div(:id, "nodiv").exist?)
   end
-
+  def test_div_click
+     assert_equal(true,@browser.text_field(:name, "text1").verify_contains("0")  )
+      @browser.div(:id, "div3").click
+    assert_equal(true, @browser.text_field(:name, "text1").verify_contains("1")  )
+  end
+  def test_inside_div
+    second_text=@browser.div(:id, "text_fields1").text_field(:index,2)
+    @browser.text_field(:name, "div_text2").set 's'
+    @browser.div(:id, "text_fields1").text_field(:index,1).set 'sample'
+    assert_equal('sample', @browser.text_field(:name, "div_text1").value)
+    assert_equal('s', second_text.value)
+  end
 end
