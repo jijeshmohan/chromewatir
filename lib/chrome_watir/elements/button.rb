919f76359dd00ef51e501d13f19d8c40931a294b
JijeshMohan <JijeshMohan@3b2c9f30-d591-11dd-86d7-074df07e0730>
2009-01-16 06:56:35 +0000

    updated button's element_type

--- lib/chrome_watir/elements/button.rb
+++ lib/chrome_watir/elements/button.rb
@@ -1,7 +1,7 @@
 module ChromeWatir
 ##This class is the chrome-watir representation of a buton
   class Button < InputElement
-    ELEMENT_TYPE = "input"
+    ELEMENT_TYPE = ["input","button"]
     INPUT_TYPES = ["button","image","submit"]    
   end
 end
