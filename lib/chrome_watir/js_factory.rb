6c8dfe6eca63feaed2a9fe71ddbe6a6c259613bd
saidesertrose2004 <saidesertrose2004@3b2c9f30-d591-11dd-86d7-074df07e0730>
2009-01-16 10:35:57 +0000

    Refactored js factory for input types array in js

--- lib/chrome_watir/js_factory.rb
+++ lib/chrome_watir/js_factory.rb
@@ -7,112 +7,157 @@ module ChromeWatir
          script=""
           case how
             when :index
-               if(defined? element.class::INPUT_TYPE)
-                    strTypes = "("
-                    element.class::INPUT_TYPE.each do |item|
-                      strTypes = strTypes +'"' + item + '",'
-                    end
-                    strTypes = strTypes.chop + ")"
-                    
-                    script = <<-EOF
-                    var foundElement = false;
-                    var selected_elements = [];
-                    var types = new Array#{strTypes};
-                    var elements = element.getElementsByTagName("#{element.class::ELEMENT_TYPE}");
-                    for (var i=0; i<elements.length;i++)
+              puts element.class::INPUT_TYPES
+              if(defined? element.class::INPUT_TYPES)
+                js_input_types = "("
+                element.class::INPUT_TYPES.each do |item|
+                  js_input_types = js_input_types +'"' + item + '",'
+                end
+                js_input_types = js_input_types.chop + ")"
+                
+                element_type = element.class::ELEMENT_TYPE
+                element_type = [element_type] unless element_type.is_a? Array 
+                js_element_types = "("
+                element_type.each do |item|
+                  js_element_types = js_element_types +'"' + item + '",'
+                end
+                js_element_types = js_element_types.chop + ")"                
+                
+                script = <<-EOF
+                var foundElement = false;
+                var selected_elements = [];
+                var elements = [];
+                var element_types = new Array#{js_element_types};
+                var input_types = new Array#{js_input_types};
+
+                for(var i=0; i<element_types.length; i++)
+                {
+                  elements.concat(element.getElementsByTagName(types[i]));
+                }
+                
+                for (var i=0; i<elements.length;i++)
+                {
+                  for (var j=0; j<input_types.length;j++)
+                  {
+                    if(elements[i].getAttribute('type') == input_types[j])
                     {
-                      for (var j=0; j<types.length;j++)
-                      {
-                        if(elements[i].getAttribute('type') == types[j])
-                        {
-                            selected_elements.push(elements[i]);
-                            foundElement=true;
-                        }
-                      }
-                    }
-                     element = selected_elements[#{what - 1}];
-                    EOF
-              else
-                    script = <<-EOF
-                    var foundElement = true;
-                    element = element.getElementsByTagName("#{element.class::ELEMENT_TYPE}")[#{what - 1}];
-                    EOF
-              end
-            when :value
-                script =<<-EOF
-                  var foundElement = false;
-                  for (var i = 0; i < element.getElementsByTagName("INPUT").length; i++) {
-                    var ele = element.getElementsByTagName("INPUT")[i];
-                    if (ele.value == '#{what}') {
-                      foundElement = true;
-                      break;
+                        selected_elements.push(elements[i]);
+                        foundElement=true;
                     }
                   }
-                  element=ele;
-                  EOF
+                }
+                 element = selected_elements[#{what - 1}];
+                EOF
+              else
+                element_type = element.class::ELEMENT_TYPE
+                element_type = [element_type] unless element_type.is_a? Array 
+                js_element_types = "("
+                element_type.each do |item|
+                  js_element_types = js_element_types +'"' + item + '",'
+                end
+                js_element_types = js_element_types.chop + ")"                
+
+                script = <<-EOF
+                var foundElement = false;
+                var elements = [];
+                var element_types = new Array#{js_element_types};
+                for(var i=0; i<element_types.length; i++)
+                {
+                  elements.concat(element.getElementsByTagName(types[i]));
+                }                
+                element = elements[#{what - 1}];
+                EOF
+              end
+            when :value
+              script =<<-EOF
+                var foundElement = false;
+                for (var i = 0; i < element.getElementsByTagName("INPUT").length; i++) {
+                  var ele = element.getElementsByTagName("INPUT")[i];
+                  if (ele.value == '#{what}') {
+                    foundElement = true;
+                    break;
+                  }
+                }
+                element=ele;
+                EOF
             when :id,:name
-                    if(defined? element.class::INPUT_TYPE)
-                      strTypes = "("
-                      element.class::INPUT_TYPE.each do |item|
-                        strTypes = strTypes +'"' + item + '",'
-                      end
-                      strTypes = strTypes.chop + ")"
-                      script = <<-EOF
-                      var foundElement = false;
-                      var selected_element = null;
-                      var types = new Array#{strTypes};
-                      var elements = element.getElementsByTagName("#{element.class::ELEMENT_TYPE}");
+              if(defined? element.class::INPUT_TYPES)
+                js_input_types = "("
+                element.class::INPUT_TYPES.each do |item|
+                  js_input_types = js_input_types +'"' + item + '",'
+                end
+                js_input_types = js_input_types.chop + ")"
+
+                element_type = element.class::ELEMENT_TYPE
+                element_type = [element_type] unless element_type.is_a? Array 
+                js_element_types = "("
+                element_type.each do |item|
+                  js_element_types = js_element_types +'"' + item + '",'
+                end
+                js_element_types = js_element_types.chop + ")"
+                
+                script = <<-EOF
+                var foundElement = false;
+                var elements = [];
+                var selected_element = null;
+                var input_types = new Array#{js_input_types};
+                var element_types = new Array#{js_element_types};
+                for(var i=0; i<element_types.length; i++)
+                {
+                  elements.concat(element.getElementsByTagName(types[i]));
+                }
+                for (var i=0; i<elements.length;i++)
+                {
+                  if(foundElement)
+                    {
+                      break;
+                    }
+                  for (var j=0; j<input_types.length;j++)
+                  {
+                    if(elements[i].getAttribute('type') == input_types[j] && elements[i].getAttribute("#{how.to_s}") == "#{what}")
+                    {
+                        selected_element=elements[i];
+                        foundElement = true;
+                        break;
+                    }
+                  }
+                }
+                 element = selected_element;
+                EOF
+            else
+              element_type = element.class::ELEMENT_TYPE
+              element_type = [element_type] unless element_type.is_a? Array 
+              strTypes = "("
+              element_type.each do |item|
+                strTypes = strTypes +'"' + item + '",'
+              end
+              strTypes = strTypes.chop + ")"
+              script = <<-EOF
+                var foundElement = false;
+                var selected_element = null;
+                var types = new Array#{strTypes};
+                 for (var j=0; j<types.length;j++)
+                {
+                    if(foundElement)
+                    {
+                      break;
+                    }
+                      var elements = element.getElementsByTagName(types[j]);
                       for (var i=0; i<elements.length;i++)
-                      {
-                        if(foundElement)
-                          {
-                            break;
-                          }
-                        for (var j=0; j<types.length;j++)
-                        {
-                          if(elements[i].getAttribute('type') == types[j] && elements[i].getAttribute("#{how.to_s}") == "#{what}")
+                      {             
+                          if(elements[i].getAttribute('#{how.to_s}') == "#{what}")
                           {
                               selected_element=elements[i];
-                              foundElement = true;
+                              foundElement=true;
                               break;
                           }
-                        }
                       }
-                       element = selected_element;
-                      EOF
-                  else
-                    element_type = element.class::ELEMENT_TYPE
-                    element_type = [element_type] unless element_type.is_a? Array 
-                    strTypes = "("
-                    element_type.each do |item|
-                      strTypes = strTypes +'"' + item + '",'
-                    end
-                    strTypes = strTypes.chop + ")"
-                    script = <<-EOF
-                      var foundElement = false;
-                      var selected_element = null;
-                      var types = new Array#{strTypes};
-                       for (var j=0; j<types.length;j++)
-                      {
-                          if(foundElement)
-                          {
-                            break;
-                          }
-                            var elements = element.getElementsByTagName(types[j]);
-                            for (var i=0; i<elements.length;i++)
-                            {             
-                                if(elements[i].getAttribute('#{how.to_s}') == "#{what}")
-                                {
-                                    selected_element=elements[i];
-                                    foundElement=true;
-                                    break;
-                                }
-                            }
-                      }
-                       element = selected_element;
-                      EOF
-                  end
+                }
+                 element = selected_element;
+                EOF
+            end
           end
+          puts script
           script
        end
       end
