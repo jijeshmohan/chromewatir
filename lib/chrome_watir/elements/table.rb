9855269d9164c6f418f84f94d1e109ba262f7527
JijeshMohan <JijeshMohan@3b2c9f30-d591-11dd-86d7-074df07e0730>
2009-02-13 02:51:35 +0000

    added table row and table cell support

--- lib/chrome_watir/elements/table.rb
+++ lib/chrome_watir/elements/table.rb
@@ -2,6 +2,7 @@ module ChromeWatir
   #This class is the chrome-watir representation of div element
   class Table < WebElement
     include Container
+    include Enumerable
      ELEMENT_TYPE = "table"
      
     def row_count
@@ -9,5 +10,61 @@ module ChromeWatir
       @container.js_eval('element.getElementsByTagName("TR").length;')
       return  @container.read_socket.to_i
     end
+
+    def length
+      self.row_count
+    end
+
+    def each
+      0.upto(self.length - 1) { |i| yield iterator_object(i) }
+    end
+    
+    def [](index)
+      assert_exist
+      return iterator_object(index-1)
+    end
+
+    def column_count(index=1)
+      assert_exist
+      iterator_object(index-1).column_count
+    end
+
+    private
+    def iterator_object(i)
+      TableRow.new(self, :index, i + 1)
+    end    
+  end
+
+  #This class is the chrome-watir representation of table row element
+  class TableRow < WebElement
+    include Container
+    include Enumerable
+     ELEMENT_TYPE = "tr"
+    def length
+      self.column_count
+    end
+    def column_count(index=1)
+      assert_exist
+      @container.js_eval('element.getElementsByTagName("TD").length;')
+      return  @container.read_socket.to_i
+    end
+    def each
+      0.upto(self.length - 1) { |i| yield iterator_object(i) }
+    end
+    
+    def [](index)
+      assert_exist
+      return iterator_object(index-1)
+    end
+    private
+    def iterator_object(i)
+      TableCell.new(self, :index, i + 1)
+    end    
+  end
+  #This class is the chrome-watir representation of table cell element
+  class TableCell < WebElement
+    include Container
+     ELEMENT_TYPE = "td"
   end
 end
+
--- test/test_table.rb
+++ test/test_table.rb
@@ -11,8 +11,13 @@ class TableTest < Test::Unit::TestCase
     assert_equal(true,@browser.table(:id, "tblTest").exist?)
     assert_equal(false,@browser.table(:id, "nodiv").exist?)
   end
- def test_table_rowcount
+ def test_table_row_count
   assert_equal(2,@browser.table(:id,"tblTest").row_count)
+  assert_equal(2,@browser.table(:id,"tblTest").length)
+  assert_equal(2,@browser.table(:id,"tblTest")[1].column_count)
  end
- 
+ def test_table_column
+  assert_equal(2,@browser.table(:id,"tblTest").column_count)
+  assert_equal('Row 1 Col1',@browser.table(:id,"tblTest")[1][1].text)
+ end
 end
